7fb9432b629217c5b033c4cd2d00a8d8206700cb
Benjamin DIDIER <36425380+Aziks0@users.noreply.github.com>
2021-12-27 14:31:43 +0100

    feat: add changeScene method

--- src/main/java/com/aziks/reader/controllers/Controller.java
+++ src/main/java/com/aziks/reader/controllers/Controller.java
@@ -3,14 +3,30 @@ package com.aziks.reader.controllers;
 import com.aziks.reader.I18n;
 import com.aziks.reader.Settings;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 abstract class Controller {
+  protected void changeScene(Stage stage, String resourcePath) throws IOException {
+    ResourceBundle bundle = I18n.getBundle();
+    FXMLLoader fxmlLoader =
+        new FXMLLoader(getClass().getResource("/com/aziks/reader" + resourcePath));
+    Scene scene = new Scene((fxmlLoader.load()));
 
+    if (Settings.getKeepWindowSize()) {
+      stage.setWidth(Settings.getWindowWidth());
+      stage.setHeight(Settings.getWindowHeight());
+      stage.setMaximized(Settings.getWindowMaximized());
+    }
+    stage.setScene(scene);
+    stage.show();
   }
 
   /**
@@ -24,9 +40,15 @@ abstract class Controller {
     return (Stage) node.getScene().getWindow();
   }
 
-  protected File getDirectory(ActionEvent event) {
-    Stage currentStage = getStage(event);
+  /**
+   * Show a directory chooser dialog
+   *
+   * @param stage A window stage
+   * @return The selected directory or null if no directory has been selected
+   * @see javafx.stage.DirectoryChooser#showDialog
+   */
+  protected File getDirectory(Stage stage) {
     DirectoryChooser dirChooser = new DirectoryChooser();
-    return dirChooser.showDialog(currentStage);
+    return dirChooser.showDialog(stage);
   }
 }
